a6fe76f11dbd6af33ed256033e2c7e167ea5e6af
kkoles <ksup4ik@gmail.com>
2018-11-26 17:30:47 +0300

    final provision

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@ Vagrant.configure("2") do |config|
     config.vm.box_check_update = false
     config.vm.define "jenkins" do |jenkins|
         jenkins.vm.hostname = "jenkins"
-        jenkins.vm.network "private_network", ip: "192.168.115.60"
+        jenkins.vm.network "private_network", ip: "192.168.10.10"
         jenkins.vm.provider "virtualbox" do |vb|  
             vb.memory = "2048"
             vb.gui = false
--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #install java and nginx
-sudo yum install -y java nginx
+sudo yum install -y java nginx epel-release
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.191.b12-0.el7_5.x86_64" | sudo tee -a ~/.bash_profile
 echo "export JRE_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.191.b12-0.el7_5.x86_64/jre" | sudo tee -a ~/.bash_profile
 
@@ -13,11 +13,17 @@ sudo chown -R jenkins:jenkins /opt/jenkins
 
 #Systemd  File
 sudo touch /etc/systemd/system/jenkins.service
-echo "[Unit]" | sudo tee -a /usr/lib/systemd/system/jenkins.service
-echo "Description=Jenkins Daemon" | sudo tee -a /usr/lib/systemd/system/jenkins.service
-echo "[Service]" | sudo tee -a /usr/lib/systemd/system/jenkins.service
-echo "ExecStart=/usr/bin/java -jar /home/jenkins/jenkins.war" | sudo tee -a /usr/lib/systemd/system/jenkins.service
-echo "User=jenkins" | sudo tee -a /usr/lib/systemd/system/jenkins.service
-echo "[Install]" | sudo tee -a /usr/lib/systemd/system/jenkins.service
-echo "WantedBy=multi-user.target" | sudo tee -a /usr/lib/systemd/system/jenkins.service
+echo "[Unit]" | sudo tee -a /etc/systemd/system/jenkins.service 
+echo "Description=Jenkins Daemon" | sudo tee -a /etc/systemd/system/jenkins.service
+echo "[Service]" | sudo tee -a /etc/systemd/system/jenkins.service
+echo "ExecStart=/usr/bin/java -Xms1500M -Xmx3000M -jar /opt/jenkins/jenkins.war" | sudo tee -a /etc/systemd/system/jenkins.service
+echo "User=jenkins" | sudo tee -a /etc/systemd/system/jenkins.service
+echo "[Install]" | sudo tee -a /etc/systemd/system/jenkins.service
+echo "WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/jenkins.service
 sudo sed -i '/^[^#].*location \/ {/a  proxy_pass    http://192.168.10.10:8080;' /etc/nginx/nginx.conf
+
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl enable nginx
+sudo systemctl start jenkins
+sudo systemctl start nginx
